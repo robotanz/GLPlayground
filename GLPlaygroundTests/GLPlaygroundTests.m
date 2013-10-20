db080fc13f551a0e63f7f7d2da1e6dc6cf216970
Thomas Jgenti <jgenti@MacBook-Pro-de-THOMAS.local>
2013-10-20 16:13:45 +0200

    Initial Commit

---
+++ GLPlaygroundTests/GLPlaygroundTests.m
@@ -0,0 +1,34 @@
+//
+//  GLPlaygroundTests.m
+//  GLPlaygroundTests
+//
+//  Created by Thomas Jgenti on 20/10/13.
+//  Copyright (c) 2013 Thomas Jgenti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GLPlaygroundTests : XCTestCase
+
+@end
+
+@implementation GLPlaygroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
